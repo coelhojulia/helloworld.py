94405c34a0ea10628dfb403a1d669cd2d28a9346
Julia Isabelly Lourenço Coelho <juisabelly48@gmail.com>
2023-02-27 16:59:40 -0300

    Estrutura condicional simples

--- main.py
+++ main.py
@@ -1,23 +1,8 @@
-salario1: float; salario2:float
-nome1: str; nome2: str
-idade: int
-sexo: str
+x = 10
 
-nome1 = input("Nome da primeira pessoa: ")
-salario1 = float(input("Salário da primeira pessoa: "))
+print("Bom dia")
 
-nome2 = input("Nome da segunda pessoa: ")
-salario2 = float(input("Salário da segunda pessoa: "))
+if x < 0:
+    print("Boa tarde")
 
-idade = int(input("Digite uma idade: "))
-sexo = input("Digite um sexo (F/M): ")
-
-print(f"Nome1: {nome1}")
-print(f"Salário1: {salario1:.2f}")
-
-print(f"Nome2: {nome2}")
-print(f"Salário2: {salario2:.2f}")
-
-print(f"Idade: {idade}")
-
-print(f"Sexo: {sexo}")
+print("Boa noite")
