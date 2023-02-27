80a786e41a55bffdbb41ad65b41d24cedac60fbf
Julia Isabelly Lourenço Coelho <juisabelly48@gmail.com>
2023-02-27 16:25:30 -0300

    Impressão de dados com interpolação

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-x: float
-x = 2.3456
-print("{:f}".format(x))
+idade: int
+salario: float
+nome: str
+sexo: str
+idade = 32
+salario = 4560.9
+nome = "Maria Silva"
+sexo = "F"
+
+print(f"A funcionária {nome}, sexo {sexo}, ganha {salario} e tem {idade} anos")
