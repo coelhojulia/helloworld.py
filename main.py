6745ff626c990f718595615a4307e24e9ef6c2ce
Julia Isabelly Lourenço Coelho <juisabelly48@gmail.com>
2023-02-28 13:56:57 -0300

    Matriz

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
-N = int(input("Quantos numeros voce vai digitar? "))
+M = int; N = int
 
-vet: [float] = [0 for x in range(N)]
+M = int(input("Quantas linhas terá a sua matriz? "))
+N = int(input("Quantas colunas terá a sua matriz? "))
 
-for i in range(0, N):
-    vet[i] = float(input("Digite um numero: "))
+mat: [[int]] = [[0 for x in range(N)] for x in range(M)]
+
+for i in range(0, M):
+    for j in range(0, N):
+        mat[i][j] =  int(input(f"Elemento [{i},{j}]: "))
 
 print()
-print("NUMEROS DIGITADOS:")
-for i in range(0, N):
-    print(f"{vet[i]:.1f}")
+print("MATRIZ DIGITADA: ")
+for i in range(0, M):
+    for j in range(0, N):
+        print(f"{mat[i][j]} ", end="")
+    print()
