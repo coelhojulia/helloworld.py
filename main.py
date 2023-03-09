a4636a83462d3ab2fc1267abcee17f26f8d2ceec
coelhojulia <juisabelly48@gmail.com>
2023-03-09 11:12:44 -0300

    Resolução 'soma impares'

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
-print("Digite dois numeros:")
+print("Digite dois numeros: ")
 x = int(input())
 y = int(input())
 
-while x != y:
-    if x < y:
-        print("CRESCENTE!")
-    else:
-        print("DECRESCENTE!")
+if x > y:
+    troca = x
+    x = y
+    y = troca
 
-    print("Digite outros dois numeros: ")
-    x = int(input())
-    y = int(input())
+soma = 0
+for i in range(x+1, y):
+    if i % 2 != 0:
+        soma = soma + i
+
+print(f"SOMA DOS IMPARES = {soma}")
